b238e14e0e65bdd059141728ce8cfdad40554c78
Ted Unangst <tedu@tedunangst.com>
2019-07-31 18:13:05 -0400

    should be more efficient to do gamma correction once per pixel

--- image/vacuum.go
+++ image/vacuum.go
@@ -100,9 +100,9 @@ func Vacuum(reader io.Reader, params Params) (*Image, error) {
 					p := newimg.Stride*j + i*4
 					q1 := oldimg.Stride*(j*2+0) + i*4*2
 					q2 := oldimg.Stride*(j*2+1) + i*4*2
-					newimg.Pix[p+0] = blend(oldimg.Pix, q1+0, q1+4, q2+0, q2+4)
-					newimg.Pix[p+1] = blend(oldimg.Pix, q1+1, q1+5, q2+1, q2+5)
-					newimg.Pix[p+2] = blend(oldimg.Pix, q1+2, q1+6, q2+2, q2+6)
+					newimg.Pix[p+0] = oldblend(oldimg.Pix, q1+0, q1+4, q2+0, q2+4)
+					newimg.Pix[p+1] = oldblend(oldimg.Pix, q1+1, q1+5, q2+1, q2+5)
+					newimg.Pix[p+2] = oldblend(oldimg.Pix, q1+2, q1+6, q2+2, q2+6)
 					newimg.Pix[p+3] = squish(oldimg.Pix, q1+3, q1+7, q2+3, q2+7)
 				}
 			}
@@ -115,48 +115,70 @@ func Vacuum(reader io.Reader, params Params) (*Image, error) {
 					p := newimg.Stride*j + i*4
 					q1 := oldimg.Stride*(j*2+0) + i*4*2
 					q2 := oldimg.Stride*(j*2+1) + i*4*2
-					newimg.Pix[p+0] = blend(oldimg.Pix, q1+0, q1+4, q2+0, q2+4)
-					newimg.Pix[p+1] = blend(oldimg.Pix, q1+1, q1+5, q2+1, q2+5)
-					newimg.Pix[p+2] = blend(oldimg.Pix, q1+2, q1+6, q2+2, q2+6)
+					newimg.Pix[p+0] = oldblend(oldimg.Pix, q1+0, q1+4, q2+0, q2+4)
+					newimg.Pix[p+1] = oldblend(oldimg.Pix, q1+1, q1+5, q2+1, q2+5)
+					newimg.Pix[p+2] = oldblend(oldimg.Pix, q1+2, q1+6, q2+2, q2+6)
 					newimg.Pix[p+3] = squish(oldimg.Pix, q1+3, q1+7, q2+3, q2+7)
 				}
 			}
 			img = newimg
 		case *image.YCbCr:
-			w, h := oldimg.Rect.Max.X/2, oldimg.Rect.Max.Y/2
+			oldw, oldh := oldimg.Rect.Max.X, oldimg.Rect.Max.Y
+			w, h := oldw/2, oldh/2
 			newimg := image.NewYCbCr(image.Rectangle{Max: image.Point{X: w, Y: h}},
 				oldimg.SubsampleRatio)
+
+			oldg := make([]float32, oldw*oldh)
+			gammaConvert(oldg, oldimg.Y, oldw, oldh, 1, 0, oldimg.YStride)
+			newg := make([]float32, w*h)
 			for j := 0; j < h; j++ {
 				for i := 0; i < w; i++ {
-					p := newimg.YStride*j + i
-					q1 := oldimg.YStride*(j*2+0) + i*2
-					q2 := oldimg.YStride*(j*2+1) + i*2
-					newimg.Y[p+0] = blend(oldimg.Y, q1+0, q1+1, q2+0, q2+1)
+					p := w*j + i
+					q1 := oldw*(j*2+0) + i*2
+					q2 := oldw*(j*2+1) + i*2
+					newg[p+0] = blend(oldg, q1+0, q1+1, q2+0, q2+1)
 				}
 			}
+			gammaRevert(newimg.Y, newg, w, h, 1, 0, newimg.YStride)
+
 			switch newimg.SubsampleRatio {
 			case image.YCbCrSubsampleRatio444:
-				w, h = w, h
+				oldw, oldh = oldw, oldh
 			case image.YCbCrSubsampleRatio422:
-				w, h = w/2, h
+				oldw, oldh = oldw/2, oldh
 			case image.YCbCrSubsampleRatio420:
-				w, h = w/2, h/2
+				oldw, oldh = oldw/2, oldh/2
 			case image.YCbCrSubsampleRatio440:
-				w, h = w, h/2
+				oldw, oldh = oldw, oldh/2
 			case image.YCbCrSubsampleRatio411:
-				w, h = w/4, h
+				oldw, oldh = oldw/4, oldh
 			case image.YCbCrSubsampleRatio410:
-				w, h = w/4, h/2
+				oldw, oldh = oldw/4, oldh/2
 			}
+			w, h = oldw/2, oldh/2
+
+			gammaConvert(oldg, oldimg.Cb, oldw, oldh, 1, 0, oldimg.CStride)
 			for j := 0; j < h; j++ {
 				for i := 0; i < w; i++ {
-					p := newimg.CStride*j + i
-					q1 := oldimg.CStride*(j*2+0) + i*2
-					q2 := oldimg.CStride*(j*2+1) + i*2
-					newimg.Cb[p+0] = blend(oldimg.Cb, q1+0, q1+1, q2+0, q2+1)
-					newimg.Cr[p+0] = blend(oldimg.Cr, q1+0, q1+1, q2+0, q2+1)
+					p := w*j + i
+					q1 := oldw*(j*2+0) + i*2
+					q2 := oldw*(j*2+1) + i*2
+					newg[p+0] = blend(oldg, q1+0, q1+1, q2+0, q2+1)
 				}
 			}
+			gammaRevert(newimg.Cb, newg, w, h, 1, 0, newimg.CStride)
+
+			gammaConvert(oldg, oldimg.Cr, oldw, oldh, 1, 0, oldimg.CStride)
+			for j := 0; j < h; j++ {
+				for i := 0; i < w; i++ {
+					p := w*j + i
+					q1 := oldw*(j*2+0) + i*2
+					q2 := oldw*(j*2+1) + i*2
+					newg[p+0] = blend(oldg, q1+0, q1+1, q2+0, q2+1)
+				}
+			}
+			gammaRevert(newimg.Cr, newg, w, h, 1, 0, newimg.CStride)
+
 			img = newimg
 		default:
 			// convert to RGBA, then loop and try again
@@ -223,7 +245,7 @@ func Vacuum(reader io.Reader, params Params) (*Image, error) {
 	return rv, nil
 }
 
-func lineate(s uint8) float64 {
+func lineate(s uint8) float32 {
 	x := float64(s)
 	x /= 255.0
 	if x < 0.04045 {
@@ -233,7 +255,7 @@ func lineate(s uint8) float64 {
 		x /= 1.055
 		x = math.Pow(x, 2.4)
 	}
-	return x
+	return float32(x)
 }
 
 func delineate(x float64) uint8 {
@@ -248,12 +270,37 @@ func delineate(x float64) uint8 {
 	return uint8(x)
 }
 
-func blend(d []byte, s1, s2, s3, s4 int) byte {
+func gammaConvert(g []float32, d []byte, w int, h int, pixsize int, chanoffset int, stride int) []float32 {
+	for j := 0; j < h; j++ {
+		for i := 0; i < w; i++ {
+			g[j*w+i] = lineate(d[j*stride+i*pixsize+chanoffset])
+		}
+	}
+	return g
+}
+
+func gammaRevert(d []byte, g []float32, w int, h int, pixsize int, chanoffset int, stride int) {
+	for j := 0; j < h; j++ {
+		for i := 0; i < w; i++ {
+			d[j*stride+i*pixsize+chanoffset] = delineate(float64(g[j*w+i]))
+		}
+	}
+}
+
+func blend(g []float32, s1, s2, s3, s4 int) float32 {
+	l1 := g[s1]
+	l2 := g[s2]
+	l3 := g[s3]
+	l4 := g[s4]
+	return ((l1 + l2 + l3 + l4) / 4.0)
+}
+
+func oldblend(d []byte, s1, s2, s3, s4 int) byte {
 	l1 := lineate(d[s1])
 	l2 := lineate(d[s2])
 	l3 := lineate(d[s3])
 	l4 := lineate(d[s4])
-	return delineate((l1 + l2 + l3 + l4) / 4.0)
+	return delineate(float64(l1+l2+l3+l4) / 4.0)
 }
 
 func squish(d []byte, s1, s2, s3, s4 int) byte {
