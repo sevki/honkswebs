653782546c72f8300a63ecd551c07afaae6232fe
Ted Unangst <tedu@tedunangst.com>
2019-10-23 18:02:07 -0400

    add a link to a more complete syntax highlighter

--- synlight/synlight.go
+++ synlight/synlight.go
@@ -14,6 +14,7 @@
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 // something like pygments in go
+// written before I discovered https://github.com/alecthomas/chroma
 package synlight
 
 import (
