3a8483d35f4ced0c4420437e15aa12dcc3bc4ac9
Ted Unangst <tedu@tedunangst.com>
2019-11-07 17:42:36 -0500

    add benchmark (minus test files)

---
+++ htfilter/bench_test.go
@@ -0,0 +1,40 @@
+package htfilter
+
+import (
+	"testing"
+	"io/ioutil"
+)
+
+func BenchmarkScrubLargeFrag(b *testing.B) {
+	data, err := ioutil.ReadFile("fragment-large.html")
+	if err != nil {
+		b.FailNow()
+		return
+	}
+	scrubeone(b, string(data))
+}
+
+func BenchmarkScrubSmallFrag(b *testing.B) {
+	data, err := ioutil.ReadFile("fragment-small.html")
+	if err != nil {
+		b.FailNow()
+		return
+	}
+	scrubeone(b, string(data))
+}
+
+func BenchmarkScrubLargeDoc(b *testing.B) {
+	data, err := ioutil.ReadFile("document-large.html")
+	if err != nil {
+		b.FailNow()
+		return
+	}
+	scrubeone(b, string(data))
+}
+
+func scrubeone(b *testing.B, src string) {
+	var htf Filter
+	for n := 0; n < b.N; n++ {
+		htf.String(src)
+	}
+}
